2c11a2e0dade256e07ee9635dcad6c222dbdb9ab
unknown <musampapane@gmail.com>
2022-08-23 14:46:58 +0200

    updated

---
+++ components/Register.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+class Register extends Component {
+    render () {
+        return (
+            <Text>EKESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS!</Text>
+        )
+    }
+}
+export default Register
