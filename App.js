9b056f6abf69f5d15d50657bcf2056f0e9064d22
dipono <joel.buti97@gmail.com>
2022-08-23 11:14:04 +0200

    update main branch

--- App.js
+++ App.js
@@ -1,39 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home'
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [SearchWord, setSearchWord] = useState('')
-
-  function handleWeather() {
-    console.log(SearchWord)
-    alert(12)
-  }
- 
-  let weather = (
-    <View style={styles.wheatherBody}>
-      <View style={styles.upperWeather}>
-        <View style={styles.upperWheatherDetails}>
-          <View style={styles.upperLeft}>
-            <Text style={styles.city}>Pretoria</Text>
-            <Text style={styles.Temperature}>12°C</Text>
-          </View>
-          <View style={styles.upperRight}>
-            <Text>Image</Text>
-            <Text style={styles.description}>clear sky</Text>
-          </View>
-        </View>
-      </View>
-    </View>
-  )
   return (
-    <View style={styles.container}>
-      <View style={styles.formGroup}>
-        <TextInput placeholder='type city, town' style={styles.controlForm} onChangeText={(e) => setSearchWord(e)} />
-        <Pressable onPress={handleWeather} style={styles.searchBtn}><Text style={styles.searchTxt}>Search</Text></Pressable>
-      </View>
-      {weather}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen
+          name="home"
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -41,39 +26,5 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgba(218, 204, 195, 0.884)',
     height: '100vh',
-  },
-  formGroup: {
-    width: '50%',
-    margin: 'auto',
-    display: 'flex',
-    flexDirection: 'column',
-    marginTop: 50
-  },
-  controlForm: {
-    height: 40,
-    borderRadius: 12,
-    textAlign: 'center',
-    fontSize: 20,
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
-  },
-  searchBtn: {
-    marginTop: 15,
-    height: 35,
-    borderRadius: 12,
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
-  },
-  searchTxt: {
-    textAlign: 'center',
-    marginTop: 5,
-    fontSize: 20,
-  },
-  wheatherBody: {
-    backgroundColor: 'rgba(0, 0, 0, 0.507)',
-    display: 'flex',
-    flexDirection: 'column',
-    width: '50%',
-    margin: 'auto',
-    color: 'black',
-    borderRadius: 9,
-  },
+  }
 });
---
+++ components/Home.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+class Home extends Component {
+    render () {
+        return (
+            <Text>Hello World!</Text>
+        )
+    }
+}
+export default Home;
