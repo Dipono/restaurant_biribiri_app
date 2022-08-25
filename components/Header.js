2438b8dfa3afc7bbfd4b0192d6c3decc1f9fa483
dipono <joel.buti97@gmail.com>
2022-08-25 13:30:42 +0200

    header initial commit

---
+++ components/Header.js
@@ -0,0 +1,92 @@
+import { StyleSheet, Text, View, Image, Modal, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            openMenu: false,
+            openCart: false,
+            openSearch: false,
+            openUser: false
+        }
+    }
+
+    menuOpen(){
+        this.setState({ openMenu: true })
+        console.log(this.state.openMenu)
+    }
+    
+    render() {
+        /* function menuOpen(){
+            setState({openMenu: true})
+            console.log('dhdhjdfhj')
+            console.log(openMenu)
+        } */
+        return (
+            <View style={styles.main}>
+                <View style={styles.logo}>
+                    <Text style={styles.logoText} >BIRIBIRI</Text>
+                </View>
+                <View style={styles.nav}>
+                    <Image source={require('../assets/images/search.png')} style={styles.img} />
+                    <Image source={require('../assets/images/circle-user-solid.svg')} style={styles.img} />
+                    <TouchableOpacity onPress={() =>this.setState({ openCart: true })}><Image source={require('../assets/images/cart-shopping-solid.svg')} style={styles.img} /></TouchableOpacity>
+                    <TouchableOpacity onPress={() =>this.setState({ openMenu: true })}><Image source={require('../assets/images/menu.png')} style={styles.img}  /></TouchableOpacity>
+                </View>
+                <Modal transparent={true} visible={this.state.openMenu}>
+                    <View style={styles.modal}>
+                        <Text>menu</Text>
+                    </View>
+                    
+                </Modal>
+                <Modal transparent={true} visible={this.state.openCart}>
+                    <View style={styles.modal}>
+                        <Text>cart</Text>
+                    </View>
+                    
+                </Modal>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main: {
+        marginTop: 25,
+        flexDirection: 'row',
+        position: 'relative',
+
+    },
+    logo: {
+        width: '75%',
+    },
+    logoText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    nav: {
+        flexDirection: 'row',
+        marginTop: 5,
+    },
+    img: {
+        width: 16,
+        height: 17,
+        marginRight: 5,
+        marginLeft: 5,
+    },
+    profile: {
+        flexDirection: 'row',
+        
+    },
+    right: {
+        flexDirection: 'row',
+        position: 'absolute',
+        right: '2%'
+    },
+    modal: {
+        backgroundColor: 'grey',
+        flex: 1
+    },
+})
+
+export default Header;
--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { Component } from 'react';
+import Header from './Header';
 class Home extends Component {
     render () {
         return (
-            <Text>Hello World!</Text>
+            <View>
+                <Header></Header>
+                <Text>Hello World!</Text>
+            </View>
+            
         )
     }
 }
