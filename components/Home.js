e147462e98f8faad57741591d6b5004efe91e032
unknown <joel.buti97@gmail.com>
2022-08-30 06:48:02 +0200

    initial redux, not completed

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { Component } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMessage} from './redux/login'
 class Home extends Component {
-    render () {
+
+    render() {
+
         return (
-            <Text>Hello World!</Text>
+            <View >
+                <Text >Welcome</Text>
+            </View>
         )
     }
 }
