42d8c43a2f0bc7897e036131dd159b98db102aca
dipono <joel.buti97@gmail.com>
2022-08-30 16:01:43 +0200

    updated home

--- components/Home.js
+++ components/Home.js
@@ -3,28 +3,58 @@ import React, { Component } from 'react';
 import Header from './Header';
 import { useDispatch, useSelector } from 'react-redux';
 import { displayProduct } from './redux/restaurentGallery'
-import { displayProduct} from '.'
+import { db } from './firebase';
+import { collection, getDocs, query } from 'firebase/firestore'
 class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { resturentProducts: [], category: [] }
+        this.state = { dataGallary: [], category: [] }
     }
 
-    async allProfucts(){
+    async allProfucts() {
+        const dbRef = collection(db, 'foodType')
+
+        const data = await getDocs(dbRef);
+        const category = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        this.setState({ category: category })
+        category.map(async (elem) => {
+            const subCollectRef = collection(db, `foodType/${elem.id}/gallery`);
+            const foodDetails = await getDocs(subCollectRef);
+            const gallery = foodDetails.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            this.setState({ dataGallary: gallery })
+        })
 
     }
+
 
     render() {
-
+        this.allProfucts()
         return (
             <View>
                 <Header></Header>
-                <Text>Hello World!</Text>
+                <View style={styles.categories}>
+                    {this.state.category.map((cat, xid) => (
+                        <View key={xid}>
+                            <Text style={styles.categoryText} onPress={() => searchProduct(cat.catName)}>{cat.category}</Text>
+                        </View>
+                    ))}
+                </View>
             </View>
-            
+
 
         )
     }
 }
+
+const styles = StyleSheet.create({
+    categories: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+    },
+    categoryText: {
+        margin: 15
+    },
+})
 export default Home;
